2601ded2bf4f86171a091613e61ba4995a72464d
Allard van Helbergen <avanhelbergen@atlassian.com>
2019-07-14 13:58:51 +1000

    Add sketch 04.

---
+++ allards_sketches/04/sketch.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var stepX;
+var stepY;
+
+function setup() {
+  createCanvas(550, 550);
+  strokeCap(SQUARE);
+}
+
+function draw() {
+  // clear out previous drawing.
+  background(255);
+  translate(width / 2, height / 2);
+
+  var circleResolution = int(map(mouseY, 0, height, 2, 80));
+  var radius = mouseX - width / 2;
+  var angle = TAU / circleResolution;
+
+  strokeWeight(mouseY / 20);
+
+  for (var i = 0; i <= circleResolution; i++) {
+    var x = cos(angle * i) * radius;
+    var y = sin(angle * i) * radius;
+    line(0, 0, x, y);
+  }
+}
+
+function keyPressed() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+}
